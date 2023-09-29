308bcb1f504ac5c62079c22eac008f6249d8fbe9
florent storme <florentstorme@gmail.com>
2023-09-29 17:51:52 +0200

    Update of StravaReader to take the API limits into account

--- strava_api/strava_reader.py
+++ strava_api/strava_reader.py
@@ -2,6 +2,9 @@ import datetime
 import json
 import pandas as pd
 from strava_client import StravaClient
+import os
+import time
+
 
 class StravaReader:
     def __init__(self, strava_api_config):
@@ -34,12 +37,14 @@ class StravaReader:
                 n_page += 1
             else:
                 continue_cond = False
-
+        n_calls = n_page
         refined_act = list()
         for this_act in activity_list:
-            act_id = this_act['id']
-            act_dict = self.client.get_activity(act_id)
-            refined_act.append({'name': act_dict["name"],
+            if n_calls < 200:
+                # Strava limits calls to 200 per 15minutes
+                act_id = this_act['id']
+                act_dict = self.client.get_activity(act_id)
+                refined_act.append({'name': act_dict["name"],
                                 'description': act_dict['description'],
                                 'distance': act_dict["distance"],
                                 'sport_type': act_dict["sport_type"],
@@ -51,6 +56,11 @@ class StravaReader:
                                 "average_speed": act_dict['average_speed'],
                                 "max_speed": act_dict['max_speed'],
                                 })
+                n_calls += 1
+            else:
+                print("Reached API limits, try again in 15minutes")
+                n_calls = 1
+                time.sleep(900)
 
         df_act = pd.DataFrame(refined_act)
         return df_act
@@ -67,3 +77,22 @@ class StravaReader:
         new_df = self.initialize_data(after=last_date, per_page=per_page)
 
         return pd.concat([sorted_df, new_df], axis=1)
+
+
+if __name__ == "__main__":
+    # All of this is hard codded right now...
+    with open('./config_strava.json', 'r') as f:
+        config_dict = json.load(f)
+
+    strava_reader = StravaReader(config_dict)
+    # Load data and store it as an excel for simplicity
+    if os.path.isfile('../data/strava.xlsx'):
+        df = pd.read_excel('../data/strava.xlsx')
+        os.rename('../data/strava.xlsx', '../data/strava_old.xlsx')
+        df = strava_reader.update_data(df)
+        df['date'] = df['date'].dt.tz_localize(None)
+        df.to_excel('../data/strava.xlsx')
+    else:
+        df = strava_reader.initialize_data(after=datetime.datetime(2023, 5, 1))
+        df['date'] = df['date'].dt.tz_localize(None)
+        df.to_excel('../data/strava.xlsx')
